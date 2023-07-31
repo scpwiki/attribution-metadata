501bcaf4177942d47e7ffa69675eb272c6b27e1d
Emmie Maeda <emmie.maeda@gmail.com>
2023-07-30 20:38:52 -0400

    Add check_password! to handle_set_page().

--- src/handlers.rs
+++ src/handlers.rs
@@ -72,6 +72,7 @@ pub async fn handle_set_page(req: Request) -> Result<(u16, String), Error> {
         page_slug,
         attributions_len = attributions.0.len(),
     );
+    check_password!(dynamo, site_slug, password, PasswordType::Regular);
 
     todo!()
 }
