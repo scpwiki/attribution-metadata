477557385700e381f60a019cacdfcfeb8d7f2e41
Emmie Maeda <emmie.maeda@gmail.com>
2023-08-02 18:31:45 -0400

    Add parameters if HEAD or GET.

--- web/editor.js
+++ web/editor.js
@@ -166,7 +166,15 @@ async function getUserInfo(name) {
 const ATTRIB_ENDPOINT = 'https://tptm7stb3j3onds27seddf2izq0mcesv.lambda-url.us-east-2.on.aws';
 
 async function queryAttrib(method, route, data = null) {
-  const body = data ? JSON.stringify(data) : undefined;
+  let body = undefined;
+  if (method === 'HEAD' || method === 'GET') {
+    if (data) {
+      route += '?' + new URLSearchParams(data);
+    }
+  } else {
+    body = JSON.stringify(data);
+  }
+
   const request = new Request(ATTRIB_ENDPOINT + route, {
     keepalive: true,
     method,
